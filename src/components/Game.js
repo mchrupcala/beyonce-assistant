c8e8f726c4336ab114623b6417a2f7f8076d92a0
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-10-20 12:29:04 -0400

    Reorganized the story logic so its in the right order.

--- src/components/Game.js
+++ src/components/Game.js
@@ -44,13 +44,11 @@ export default function Game() {
         <p>{"Click here if you chose to dry off."}</p>
         <p>{"Click here if you chose to take a shower and keep the stylist team waiting."}</p>
 
-{/*FAILED TITLE 4*/}
-<h3>{"The application for the makeup and body makeup is ruined from the chlorine. You\'ve been FIRED."}</h3>
+        {/*FAILED TITLE 4*/}
+        <h3>{"The application for the makeup and body makeup is ruined from the chlorine. You\'ve been FIRED."}</h3>
 
-{/*FAILED TITLE 4*/}
-<h3>{"Beyonce\'s hair is WET causing even more time making her late to the event. You\'ve been fired."}</h3>
-
-
+        {/*FAILED TITLE 4*/}
+        <h3>{"Beyonce\'s hair is WET causing even more time making her late to the event. You\'ve been fired."}</h3>
 
 {/*TITLE 4b -- PASSED TITLE 3 */}
         <h3>{"She paints."}</h3>
@@ -58,107 +56,83 @@ export default function Game() {
         <p>{"Click here if you chose Sandcastles."}</p>
         <p>{"Click here if you chose All Night."}</p>
 
-{/*FAILED TITLE 4b*/}
-        <h3>{"She starts crying then fires you after the 3:03 minutes."}</h3>
+        {/*(Sandcastles) FAILED TITLE 4b*/}
+                <h3>{"She starts crying then fires you after the 3:03 minutes."}</h3>
 
 
 
-{/*TITLE 5 -- PASSED TITLE 4b */}
+{/*(All Night )TITLE 5 -- PASSED TITLE 4b */}
         <h3>{"She says 'Oh yes that\'s the one.' and sings along while painting."}</h3>
         <h3>{"Beyonce is running late should she go to the hotel and get ready or do it in the car?"}</h3>
         <p>{"Click here if she should get ready in the car."}</p>
         <p>{"Click here if she should go to the hotel and get ready."}</p>
 
-{/*FAILED TITLE 5*/}
-        <h3>{"She got car sick and threw up all over her outfit. You\'ve been kicked out the car and fired."}</h3>  
+        {/*FAILED TITLE 5*/}
+                <h3>{"She got car sick and threw up all over her outfit. You\'ve been kicked out the car and fired."}</h3>  
 
-        
+
 
 {/*TITLE 6 -- PASSED TITLE 5 */}
         <h3>{"She\'s arrived at the hotel but the paparazzi have already spotted her Mercedes Sprinter. Should she risk being spotted at this event that she\'s supposed to make a surprise appearance or run late and go to another hotel?"}</h3>
         <p>{"Click here if she should risk being spotted."}</p>
         <p>{"Click here if she should run late and go to another hotel."}</p>
 
+
+
 {/*TITLE 7a -- PASSED TITLE 6 */}
         <h3>{"She was thankfully not been spotted, she's making her way to the presidential suite 10 floors upstairs should she ditch everyone and go by herself so she doesn't draw attention or take the risk and go with her team?"}</h3>
         <p>{"Click here if she should go alone to the presidential suite."}</p>
         <p>{"Click here if she should go with her team to the presidential suite."}</p>
 
+        {/*FAILED TITLE 7a*/}
+                <h3>{"Beyonce was kidnapped so Beyonce's other assistant fires you."}</h3>  
+
+        {/*FAILED TITLE 7a*/}
+                <h3>{"She's been spotted and you ruin a whole era so she fires you."}</h3>  
+
 {/*TITLE 7b -- PASSED TITLE 6 */}
         <h3>{"The west family offer her to join them in their suite and Motel 6 is able to provide a whole floor. Which do you suggest?"}</h3>
-        <p>{"Click here"}</p>
-        <p>{"Click here"}</p>
+        <p>{"Click here if you chose the West suite."}</p>
+        <p>{"Click here if you chose the Motel 6."}</p>
 
-{/*FAILED TITLE 7a*/}
-        <h3>{"Beyonce was kidnapped so Beyonce's other assistant fires you."}</h3>  
+        {/*(West) FAILED TITLE 7b*/}
+                <h3>{"Beyonce says nothing. Julius has fired you."}</h3>  
 
-{/*FAILED TITLE 7a*/}
-        <h3>{"She's been spotted and you ruin a whole era so she fires you."}</h3>  
 
-{/*FAILED TITLE 7b*/}
-        <h3>{"Beyonce says nothing. Julius has fired you."}</h3>  
 
-{/*TITLE 8 -- PASSED TITLE 7b */}
-        <h3>{"Beyonce says 'you know me so well'"}</h3>
+{/*(Motel 6) TITLE 8 -- PASSED TITLE 7b */}
+        <h3>{"Beyonce says 'You know me so well.'"}</h3>
         <h3>{"Beyonce asks you to tell her lawyers who needs a cease and desist who you telling them about?Beycapital or Bey_Legion?"}</h3>
-        <p>{"Click here if you chose BeyCapital"}</p>
-        <p>{"Click here if you chose Bey_Legion"}</p>
+        <p>{"Click here if you chose BeyCapital."}</p>
+        <p>{"Click here if you chose Bey_Legion."}</p>
 
-{/*TITLE 8a -- PASSED TITLE 7 */}
-        <h3>{"Her lawyers look into it and see how much money they've been profiting and she gives it to you as a bonus."}</h3>
-        <h3>{"Anyways congrats if you've made it this far for the first portion. You're minutes away from the hotel what changes do you need to have made before Beyonce arrives. Red toilet paper, newly painted walls, titanium drinking straws, or 100% cotton uniforms for the staff."}</h3>
-        <p>{"Click here if you chose titanium drinking straws."}</p>
-        <p>{"Click here if you chose freshly painted walls."}</p>
-        <p>{"Click here if you chose red toilet paper."}</p>
-        <p>{"Click here if you chose 100% cotton uniforms."}</p>
 
-{/*TITLE 8b -- PASSED TITLE 7 */}
+
+{/*TITLE 9b -- PASSED TITLE 8 */}
         <h3>{"Her secret twitter home is filled with tweets saying 'Fuck Beyonce' she thanks you and laughs."}</h3>
         <h3>{"Beyonce asks if you know any other ways to piss off the beyhive what do you suggest? What do you suggest no FWT or more ugly merch?"}</h3>
         <p>{"Click here if you chose more ugly merch."}</p>
         <p>{"Click here if you chose no FWT."}</p>
 
-{/*FAILED TITLE 8b*/}
-        <h3>{"You've offended her, she fires you."}</h3>  
+        {/*(Ugly) FAILED TITLE 9b*/}
+                <h3>{"You've offended her, she fires you."}</h3>  
 
-{/*PASSED TITLE 8b...back to 8a */}
-        <h3>{"Beyonce jots that down in her notes and sends an email to her parkwood team."}</h3>
-        <h3>{"You're minutes away from the hotel what changes do you need to have made before Beyonce arrives. Red toilet paper, newly painted walls, titanium drinking straws, or 100% cotton uniforms for the staff."}</h3>
+
+{/*TITLE 9a -- PASSED TITLE 8 */}
+        {/* (FWT) */}<h3>{"Beyonce jots that down in her notes and sends an email to her parkwood team."}</h3>
+        {/* (Capital) */}<h3>{"Her lawyers look into it and see how much money they've been profiting and she gives it to you as a bonus."}</h3>
+        <h3>{"Anyways congrats if you've made it this far for the first portion. You're minutes away from the hotel what changes do you need to have made before Beyonce arrives. Red toilet paper, newly painted walls, titanium drinking straws, or 100% cotton uniforms for the staff."}</h3>
         <p>{"Click here if you chose titanium drinking straws."}</p>
         <p>{"Click here if you chose freshly painted walls."}</p>
         <p>{"Click here if you chose red toilet paper."}</p>
-        <p>{"Click here if you chose 100% cotton uniforms."}</p>  {/*passes, continue to Title 9...write my own pass message.*/}
+        <p>{"Click here if you chose 100% cotton uniforms."}</p> {/*passes, continue to Title 9...write my own pass message.*/}
 
-        {/*FAILED TITLE 8a*/}
+        {/*(Paint) FAILED TITLE 9a*/}
         <h3>{"Beyonce has passed out from breathing in the fresh paint. You're fired."}</h3>  
 
-{/*TITLE 9 -- PASSED TITLE 8a */}
-        <h3>{"Beyonce takes back the bonus for you spending $800 on straws."}</h3>
-        <h3>{"Beyonce's laptop went missing! What are you doing? Looking for it yourself or letting the hotel beware it's missing or both?"}</h3>
-        <p>{"Click here if you chose looking for it yourself."}</p>
-        <p>{"Click here if you chose letting the hotel beware of the missing laptop."}</p>
-        <p>{"Click here if you chose to look for it and let the hotel know the laptop is missing."}</p>
 
-{/*FAILED TITLE 9*/}
-<h3>{"You've let the hotel beware and one of the motel 6 employees stole it and leaked Byeonce's work and private information. You're fired."}</h3> 
 
-{/*TITLE 10a -- PASSED TITLE 9 */}
-        <h3>{"Where are you looking for it, the first hotel floor or the car?"}</h3>
-        <p>{"Click here if you chose the hotel floor."}</p>
-        <p>{"Click here if you chose the car."}</p>
-
-{/*FAILED TITLE 10a*/}
-<h3>{"You've been running around drawing attention, Beyonce has been spotted. You're fired."}</h3> 
-
-{/*TITLE 10b -- PASSED TITLE 9 */}
-        <h3>{"Someone has found it but are blackmailing you for money or they'll leak everything on it. What are you doing telling Beyonce or trying to negotiate with them?"}</h3>
-        <p>{"Click here if you chose to tell Beyonce."}</p>
-        <p>{"Click here if you chose to negotiate with them."}</p>
-
-{/*FAILED TITLE 10b*/}
-<h3>{"It was a trick to see if you would tell Beyonce that someone was threatening to leak her stuff on her laptop. You've been fired."}</h3> 
-  
-{/*TITLE 10c -- PASSED TITLE 9 */}
+{/*(Red) TITLE 10a -- PASSED TITLE 9a */}
         <h3>{"She hasn't went to the bathroom yet so she hasn't noticed the red toilet paper."}</h3>
         <h3>{"Beyonce says she's a little hungry. What are you getting her fruit, deli tray, juicy baked chicken, or desert?"}</h3>
         <p>{"Click here if you chose desert."}</p>
@@ -166,50 +140,80 @@ export default function Game() {
         <p>{"Click here if you chose juicy baked chicken."}</p>
         <p>{"Click here if you chose fruit."}</p>
 
-{/*(Dessert) FAILED TITLE 10c*/}
-<h3>{"Beyonce yells at you for trying to tempt her to break her diet and fires you."}</h3> 
+        {/*(Dessert) FAILED TITLE 10a*/}
+        <h3>{"Beyonce yells at you for trying to tempt her to break her diet and fires you."}</h3> 
 
-{/*(Deli Tray) FAILED TITLE 10c*/}
-<h3>{"Beyonce throws the tray at you then fires you."}</h3> 
+        {/*(Deli Tray) FAILED TITLE 10a*/}
+        <h3>{"Beyonce throws the tray at you then fires you."}</h3> 
 
-{/*(Fruit) PASSED! Go to 11b*/}
-        <h3>{"Beyonce depressingly eats the fruit."}</h3>
-        <p>{"Click here"}</p>
-        <p>{"Click here"}</p>
+        {/*(Fruit) PASSED! Continue on to 12b*/}
+                <h3>{"Beyonce depressingly eats the fruit."}</h3>
 
-{/*TITLE 11c -- PASSED TITLE 10c */}
-        <h3>{"Beyonce cheats her diet she asks you not to tell."}</h3>
-        <h3>{"Beyonce is almost ready for the event what should you make sure she gets done before she leaves? Taking pictures for Instagram or going over her speech?"}</h3>
-        <p>{"Click here if you chose taking pictures on Istagram."}</p>
-        <p>{"Click here for going over her speech."}</p>
+        {/*(Chicken) TITLE 11 -- PASSED TITLE 10a */}
+                <h3>{"Beyonce cheats her diet she asks you not to tell."}</h3>
+                <h3>{"Beyonce is almost ready for the event what should you make sure she gets done before she leaves? Taking pictures for Instagram or going over her speech?"}</h3>
+                <p>{"Click here if you chose taking pictures on Istagram."}</p>
+                <p>{"Click here for going over her speech."}</p>
 
-{/*FAILED TITLE 11c*/}
-<h3>{"Beyonce has to use the bathroom and fires you for thinking she uses red toilet paper."}</h3> 
+                {/*(Either) FAILED TITLE 11*/}
+                <h3>{"Beyonce has to use the bathroom and fires you for thinking she uses red toilet paper."}</h3>
 
 
-{/*TITLE 11a -- PASSED TITLE 10a */}
-        <h3>{"It was in the car."}</h3>
-        <h3>{"Beyonce asks you what color lighting would look cute with her outfit, what color you picking blue or white?"}</h3>
-        <p>{"Click here if you chose blue lighting."}</p>
-        <p>{"Click here if you chose white lighting."}</p>
 
-{/*Chose White Lighting -- YOU WIN!! */}
-<h3>{"It's time for the event!"}</h3>
 
-{/*FAILED TITLE 11a*/}
-<h3>{"Beyonce says 'Do you even know me?' then fires you."}</h3>
+{/*(Straws) TITLE 10b -- PASSED TITLE 9a */}
+        <h3>{"Beyonce takes back the bonus for you spending $800 on straws."}</h3>
+        <h3>{"Beyonce's laptop went missing! What are you doing? Looking for it yourself or letting the hotel beware it's missing or both?"}</h3>
+        <p>{"Click here if you chose looking for it yourself."}</p>
+        <p>{"Click here if you chose letting the hotel beware of the missing laptop."}</p>
+        <p>{"Click here if you chose to look for it and let the hotel know the laptop is missing."}</p>
 
-{/*TITLE 11b -- PASSED TITLE 10b */}
-        <h3>{"Beyonce's team handles it and nods and says thank you."}</h3>
-        <h3>{"Beyonce asks if you made sure the event organizer had her seated away from a certain someone. Who is she referring to? Tiffany Haddish or Nicki Minaj?"}</h3>
-        <p>{"Click here if you chose Nicki Minaj."}</p>
-        <p>{"Click here if you chose Tiffany Haddish."}</p>
+        {/*FAILED TITLE 10b*/}
+        <h3>{"You've let the hotel beware and one of the motel 6 employees stole it and leaked Byeonce's work and private information. You're fired."}</h3> 
 
-{/*Chose Tiffany Haddish -- YOU WIN!! */}
-        <h3>{"It's time for the event!"}</h3>
+        {/*TITLE 11a -- PASSED TITLE 10b */}
+                <h3>{"Where are you looking for it, the first hotel floor or the car?"}</h3>
+                <p>{"Click here if you chose the hotel floor."}</p>
+                <p>{"Click here if you chose the car."}</p>
 
-{/*FAILED TITLE 11b*/}
-<h3>{"Nicki Minaj texts her from the event that she's seated way in the back, and Beyonce fires you. "}</h3>
+                {/*(Hotel) FAILED TITLE 11a*/}
+                <h3>{"You've been running around drawing attention, Beyonce has been spotted. You're fired."}</h3> 
+
+                {/*TITLE 12a -- PASSED TITLE 11a */}
+                        <h3>{"It was in the car."}</h3>
+                        <h3>{"Beyonce asks you what color lighting would look cute with her outfit, what color you picking blue or white?"}</h3>
+                        <p>{"Click here if you chose blue lighting."}</p>
+                        <p>{"Click here if you chose white lighting."}</p>
+
+                        {/*FAILED TITLE 12a*/}
+                        <h3>{"Beyonce says 'Do you even know me?' then fires you."}</h3>
+
+                        {/*Chose White Lighting -- YOU WIN!! */}
+                        <h3>{"It's time for the event!"}</h3>
+
+
+
+
+        {/*TITLE 11b -- PASSED TITLE 10b */}
+                <h3>{"Someone has found it but are blackmailing you for money or they'll leak everything on it. What are you doing telling Beyonce or trying to negotiate with them?"}</h3>
+                <p>{"Click here if you chose to tell Beyonce."}</p>
+                <p>{"Click here if you chose to negotiate with them."}</p>
+
+                {/*(Negotiate) FAILED TITLE 11b*/}
+                <h3>{"It was a trick to see if you would tell Beyonce that someone was threatening to leak her stuff on her laptop. You've been fired."}</h3> 
+                
+
+                {/*TITLE 12b -- PASSED TITLE 11b */}
+                        <h3>{"Beyonce's team handles it and nods and says thank you."}</h3>
+                        <h3>{"Beyonce asks if you made sure the event organizer had her seated away from a certain someone. Who is she referring to? Tiffany Haddish or Nicki Minaj?"}</h3>
+                        <p>{"Click here if you chose Nicki Minaj."}</p>
+                        <p>{"Click here if you chose Tiffany Haddish."}</p>
+
+                        {/*FAILED TITLE 12b*/}
+                        <h3>{"Nicki Minaj texts her from the event that she's seated way in the back, and Beyonce fires you. "}</h3>
+
+                        {/*Chose Tiffany Haddish -- YOU WIN!! */}
+                                <h3>{"It's time for the event!"}</h3>
 
 
 
