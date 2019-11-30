4a966951cbdf7e70ebf99adaa545cdc626ce30cd
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-11-30 13:57:59 -0500

    Wrote all the groundwork for a pop-up welcome banner. Added toggle functionality. All thats left is basic styling.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Nav from './components/Nav';
 const App = props => {
   const [index, setIndex] = useState(0);
   const [clicked, setClicked] = useState(false);
+  const [bannerClick, setBannerClick] = useState(false);
 
 
   let clickHandler = (item) => {
@@ -18,8 +19,19 @@ const App = props => {
     props.nextQuestion(item);
   }
 
+  let bannerHandler = () => {
+    setBannerClick(true);
+  }
+
   return (
     <div className="App">
+        <div className={bannerClick ? 'hide-banner' : "welcome-banner"} >
+          <div className="banner-content">
+            <h2>Think you have what it takes to be Bey's assistant for the day?</h2>
+            <button className="banner-button" onClick={bannerHandler}>Yes/No?</button>
+          </div>
+        </div>
+
       <Nav score={props.score}/>
       <header className="App-header">
         <h1>Beyonce's Assistant</h1>
@@ -28,6 +40,9 @@ const App = props => {
       <div>Inspired by Landon Rivera's  
         <a href="https://twitter.com/CORNYASSBITCH/status/1142591156884127744">Twitter game.</a>
       </div>
+
+
+
     </div>
   );
 }
